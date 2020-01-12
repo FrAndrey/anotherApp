a2933a6abf919ef06b2bfc3a9e30922779343dcf
FrAndrey <45703899+FrAndrey@users.noreply.github.com>
2020-01-11 20:51:10 -0500

    Initial Commit

---
+++ anotherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  anotherApp
+//
+//  Created by Andrey on 2020-01-11.
+//  Copyright © 2020 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
